0fc83339851664a09a816fea1fec0d6a17a0b09c
Syrtsevnikita <145428432+SyrNik24@users.noreply.github.com>
2023-11-02 21:30:02 +0300

    Add files via upload

---
+++ работа с массивами.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+const int MAS1 = 5;
+const int MAS2 = 10;
+int mas1[MAS1];
+int mas2[MAS2];
+
+void random();
+
+int main()
+{
+    setlocale(0, "");
+    
+    random();
+    
+    for (int i = 0;i < MAS1;i++) {
+
+        cout << setw(5) << mas1[i];
+
+    }
+
+    cout << endl;
+
+    for (int i = 0;i < MAS2;i++) {
+
+        cout << setw(5) << mas2[i];
+
+    }
+
+    return 0;
+}
+
+void random()
+{
+    setlocale(0, "");
+
+    srand(time(0));
+
+    for (int i = 0;i < MAS1;i++) {
+
+        mas1[i] = rand() % 100 + 1;
+
+    }
+
+    for (int i = 0;i < MAS2;i++) {
+
+        mas2[i] = rand() % 100 + 1;
+
+    }
+    
+}
---
+++ работа с массивами1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int t = 0;
+int n;
+
+int calculation();
+void dowhile();
+
+int main()
+{
+    setlocale(0, "");
+
+    cout << "Введите число"<<endl;
+    cin >> n;
+    
+    calculation();
+    
+    
+    do{
+        dowhile();
+       
+    } while (t % 10==0);
+
+    cout << t << endl;
+
+    return 0;
+}
+
+int calculation()
+{
+    t = 0;
+    while (n > 0) {
+        t += n % 10;
+        n /= 10;
+    }
+    
+
+    return n;
+}
+
+void dowhile()
+{
+    do {
+        cout << t << endl;
+        n = t;
+        calculation();
+
+    } while (n % 10);
+}
